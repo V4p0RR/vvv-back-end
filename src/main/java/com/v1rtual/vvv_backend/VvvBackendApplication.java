e74f9d70aba56e2f3b099ea157f20a3f5d717790
V1rtual <whz1006549138@gmail.com>
2025-12-24 12:17:25 +0800

    yep

---
+++ src/main/java/com/v1rtual/vvv_backend/VvvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.v1rtual.vvv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VvvBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/v1rtual/vvv_backend/VvvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1rtual.vvv_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
